4c5606f392746d66e5bb29f0ee3dd607086b1269
Simon Fuet <simon.fuet@ephesia-consult.com>
2019-11-08 15:38:22 +0100

    Start OpenClassroom course

---
+++ demo/MonAppli/src/main/java/org/example/demo/App.java
@@ -0,0 +1,28 @@
+package org.example.demo;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) throws IOException {
+		System.out.println("Hello World!");
+
+		Properties vProp = new Properties();
+		InputStream vInputStream = null;
+		try {
+			vInputStream = App.class.getResourceAsStream("/info.properties");
+			vProp.load(vInputStream);
+		} finally {
+			if (vInputStream != null) {
+				vInputStream.close();
+			}
+		}
+
+		System.out.println("Application version: " + vProp.getProperty("org.example.demo.version", "?"));
+	}
+}
