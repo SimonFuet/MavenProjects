ad6e87a062615c6ae41557c9752e996c0fa3a2a8
SimonFuet <simonfuet@gmail.com>
2019-12-20 12:01:57 +0100

    Final course project

---
+++ OCFinalProject/ticket/ticket-model/src/main/java/fr/fuet/ocfinalproject/model/App.java
@@ -0,0 +1,13 @@
+package fr.fuet.ocfinalproject.model;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
